a6b9a4453a69dbf283582e0979ee9e10bc712cf4
RicardoScript <ricardo.alvarado0728@gmail.com>
2025-11-21 22:32:36 -0500

    fixed SharedPreferences

--- lib/provider/contador_provider.dart
+++ lib/provider/contador_provider.dart
@@ -2,20 +2,24 @@ import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ContadorProvider extends ChangeNotifier {
-  int _contador = 0;
+  int _contador = 1; // AHORA EMPIEZA EN 1
 
   int get contador => _contador;
 
+  // creamos un constructor para cargar el estado del contador
   ContadorProvider() {
     _cargarValor();
   }
 
+  // usamos SharedPreferences para cargar el estado del contador
+  // notifyListener sirve para notificar a los widgets que el estado ha cambiado
   Future<void> _cargarValor() async {
     final prefs = await SharedPreferences.getInstance();
-    _contador = prefs.getInt('contador') ?? 0;
+    _contador = prefs.getInt('contador') ?? 1; // VALOR POR DEFECTO 1
     notifyListeners();
   }
 
+  // usamos SharedPreferences para guardar el estado del contador al cerrar la app
   Future<void> _guardarValor() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setInt('contador', _contador);
@@ -28,13 +32,16 @@ class ContadorProvider extends ChangeNotifier {
   }
 
   void decrementar() {
-    _contador--;
-    _guardarValor();
-    notifyListeners();
+    if (_contador > 1) {
+      // NO DEJA BAJAR DE 1
+      _contador--;
+      _guardarValor();
+      notifyListeners();
+    }
   }
 
   void reiniciar() {
-    _contador = 0;
+    _contador = 1; // RESET EN 1
     _guardarValor();
     notifyListeners();
   }
