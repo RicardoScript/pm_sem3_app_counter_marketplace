7e774eb216a33852d9f0b8c77307935a18f1c2e4
RicardoScript <ricardo.alvarado0728@gmail.com>
2025-11-21 22:35:43 -0500

    importacion del archivo main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pm_sem3_app_counter_marketplace/core/main.dart';
+import 'package:pm_sem3_app_counter_marketplace/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
