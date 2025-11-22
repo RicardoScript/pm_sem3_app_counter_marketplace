62751f3a3f5c1086bf5d8b84c22e5edd432c70f6
RicardoScript <ricardo.alvarado0728@gmail.com>
2025-11-21 22:36:12 -0500

    home_screen disenio

--- lib/provider/pages/home_screen.dart
+++ lib/provider/pages/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../contador_provider.dart';
 import 'detalle_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,52 +6,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final contador = context.watch<ContadorProvider>();
-
     return Scaffold(
       appBar: AppBar(title: const Text('Contador Marketplace')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Valor actual:', style: TextStyle(fontSize: 20)),
-            Text(
-              contador.contador.toString(),
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: contador.incrementar,
-                  child: Text('+'),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: contador.decrementar,
-                  child: Text('-'),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: contador.reiniciar,
-                  child: Text('Reset'),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => DetalleScreen(valor: contador.contador),
-                  ),
-                );
-              },
-              child: Text('Ver Detalle'),
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const DetalleScreen()),
+            );
+          },
+          child: const Text('Ver Detalle'),
         ),
       ),
     );
